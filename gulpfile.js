90d16b2a7fb1ff3d650ede8b2686f30efbda03c6
Kenneth Black and Kevin Finley <kevin@>
2017-01-02 14:18:36 -0800

    add concat and browserify tasks

--- gulpfile.js
+++ gulpfile.js
@@ -7,3 +7,26 @@ var utilities = require('gulp-util');
 var del = require('del');
 var jshint = require('gulp-jshint');
 var buildProduction = utilities.env.production;
+
+// concatScripts
+gulp.task('concatScripts', function() {
+  return gulp.src(['./js/*-interface.js'])
+    .pipe(concat('allConcat.js'))
+    .pipe(gulp.dest('./tmp'))
+});
+
+// browserify *bundle
+gulp.task('jsBrowserify', ['concatScripts'], function() {
+  return browserify({entries: ['./tmp/allConcat.js']})
+    .bundle()
+    .pipe(source('app.js'))
+    .pipe(gulp.dest('./build/js'))
+})
+// minifyScripts (for production)
+
+// clean
+// build
+
+// stream
+
+// jshint
---
+++ js/memory-interface.js
@@ -0,0 +1,2 @@
+// moon-o, first-order(black on red), rebel(black on blue), star(white on ..), empire(white on black)
+//
